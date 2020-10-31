f8a6ff66c78e77f19217f1e8aa82932b81a8e257
Cristian Valles Pereira <valles.cristian1992@gmail.com>
2020-10-31 03:09:54 -0300

    feat: call simple_quote query to generate operating cost

--- app/controllers/operating_cost_controller.rb
+++ app/controllers/operating_cost_controller.rb
@@ -3,13 +3,25 @@ class OperatingCostController < ApplicationController
   end
 
   def create
-    invoice = params[:invoice] 
+    invoice = params[:invoice]
+    @operation_cost = generate_operating_cost(invoice)
   end
 
   private 
-  def generate_operating_cost 
+  def generate_operating_cost(invoice)
+    url = "https://chita.cl/api/v1/pricing/simple_quote"
+    query = {
+      "client_dni" => invoice["client_dni"],
+      :debtor_dni => invoice["debtor_dni"],
+      :document_amount => invoice["document_amount"],
+      :folio => invoice["folio"],
+      :expiration_date => invoice["expiration_date"]
+    }
+    headers = {
+      "X-APi-key" => "UVG5jbLZxqVtsXX4nCJYKwtt"
+    }
+
+    HTTParty.get(url, :headers => headers, :query => query)
   end
-
-
   
 end
